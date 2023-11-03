75941fd50361cb943e1f95db0b45500e753a61af
Jhony <Jhonny14_1@hotmail.com>
2023-11-03 14:21:29 -0500

    feat: Actualiza metodo .show() en console.py

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ HBNBCommand(cmd.Cmd)
 """
 import cmd
 from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -12,7 +13,13 @@ class HBNBCommand(cmd.Cmd):
     Esta clase hereda de cmd.Cmd, desde aqui se gestionará
     nuestro proyecto.
     """
+
     prompt = "(hbnb) "
+    __BASE_MODEL = "BaseModel"
+    __CLS_NAME_MISSING = "** class name missing **"
+    __CLS_NOT_EXIST = "** class doesn't exist **"
+    __ID_MISSING = "** instance id missing **"
+    __NO_INST_FOUND = "** no instance found **"
 
     def do_quit(self, arg):
         """Quit command to exit the program\n"""
@@ -28,16 +35,29 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, arg):
 
         if not arg:
-            print("** class name missing **")
-        elif arg == "BaseModel":
-            bs = eval(f"{arg}()")
+            print(HBNBCommand.__CLS_NAME_MISSING)
+        elif arg == HBNBCommand.__BASE_MODEL:
+            bs = BaseModel()
             bs.save()
             print(f"{bs.id}")
         else:
-            print("** class doesn't exist **")
+            print(HBNBCommand.__CLS_NOT_EXIST)
 
     def do_show(self, arg):
-        pass
+        args = arg.split()
+
+        if not args:
+            print(HBNBCommand.__CLS_NAME_MISSING)
+        elif args[0] != HBNBCommand.__BASE_MODEL:
+            print(HBNBCommand.__CLS_NOT_EXIST)
+        elif args[0] == HBNBCommand.__BASE_MODEL and args[1]:
+            objs = storage.all()
+            for obj in objs.values():
+                if obj.id == args[1]:
+                    print(obj)
+                    break
+            else:
+                print(HBNBCommand.__NO_INST_FOUND)
 
     def do_destroy(self, arg):
         pass
